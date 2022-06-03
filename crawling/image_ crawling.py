f54ad10d49cf7b73c53700bda8d94eb7495a2fb0
Lee JaeHyeong <leejh7830@naver.com>
2022-06-03 12:50:27 +0900

    220603
    gui수정 다운로드경로수정

--- crawling/image_ crawling.py
+++ crawling/image_ crawling.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 import urllib.request
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver.common.by import By
 import pyautogui
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import *
 
 form_class = uic.loadUiType("./crawling/CrawlingGui.ui")[0] # UI 불러오기
 class WindowClass(QMainWindow, form_class) :
@@ -21,35 +19,39 @@ class WindowClass(QMainWindow, form_class) :
         self.initUI()
 
     def initUI(self):
-        exitAction = QAction(QIcon('./src/exit.png'), '종료(Exit)', self)
-        # exitAction.setShortcut('Ctrl+C') # Ctrl+C 로 종료가능
+        saveAction = QAction('저장(Save)', self)
+        loadAction = QAction('불러오기(Load)', self)
+        exitAction = QAction(QIcon('crawling/src/exit.png'), '종료(Exit)', self)
+        exitAction.setShortcut('Ctrl+C') # Ctrl+C 로 종료가능
         exitAction.setStatusTip('프로그램을 종료합니다.') # 마우스 올렸을 때 도움말(화면아래쪽표시)
         exitAction.triggered.connect(qApp.quit)
+        randImg = QAction('랜덤이미지 뽑기',self)
+        help = QAction('도움말(Help)',self)
+        
 
-        # insertDog = QAction('개(Dog)',self)
-        # insertDog.setStatusTip('검색하고 싶은 단어를 클릭하세요.')
-        # insertDog.triggered.connect(qApp.inputDog)
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File') # & : F에 단축키 설정 (Alt+F)
+        fileMenu.addAction(saveAction)
+        fileMenu.addAction(loadAction)
         fileMenu.addAction(exitAction)
-        # insertMenu = menubar.addMenu('&Insert')
-        # insertMenu.addAction(insertDog)
-
-    def inputDog(self):
-        textBox_word = "Dog"
-
-
+        insertMenu = menubar.addMenu('&Insert')
+        insertMenu.addAction(randImg)
+        helpMenu = menubar.addMenu('&Help')
+        helpMenu.addAction(help)
 
     def setClickEvent(self):
         self.ok_btn.clicked.connect(self.startFunc) # 확인버튼 클릭 시 연결
         self.clear_btn.clicked.connect(self.clearFunc)
           
     def startFunc(self):
-        word = self.textBox_word.toPlainText() # toPlain : 값 가져오기
-        count = int(self.textBox_count.toPlainText())
+        word = self.wordBox.toPlainText() # toPlain : 값 가져오기
+        count = int(self.countBox.toPlainText())
         
-        driver = webdriver.Chrome()
+        # driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        driver = webdriver.Chrome(options=options)
         driver.get('http://www.google.co.kr/imghp?hl=ko')
         window = pyautogui.getActiveWindow() # 활성화된 창
         try:
@@ -72,7 +74,7 @@ class WindowClass(QMainWindow, form_class) :
         browser.send_keys(Keys.RETURN)
         time.sleep(5)
 
-        print('검색어입력, 리턴까지 완료')
+        print('검색어입력, 리턴')
 
         def selenium_scroll_option():
         # 스크롤 높이 가져옴
@@ -108,10 +110,11 @@ class WindowClass(QMainWindow, form_class) :
             print('리스트에 저장 완료')
 
             for i, url in enumerate(images_url, 1):
-                urllib.request.urlretrieve(url, 'C:\\Users\\Lee\\Desktop\\DEV\\Python\\python_work\\crawling\\download_img\\'+word+'_'+str(i)+'.jpg')
+                # urllib.request.urlretrieve(url, 'C:\\Users\\Lee\\Desktop\\DEV\\Python\\python_work\\crawling\\download_img\\'+word+'_'+str(i)+'.jpg')
+                urllib.request.urlretrieve(url, 'crawling\\download_img\\'+word+'_'+str(i)+'.jpg')
 
             print('다운로드 완료')
-            
+           
                     
 
         if count >= 50:
@@ -130,8 +133,8 @@ class WindowClass(QMainWindow, form_class) :
 
         
     def clearFunc(self):
-        self.textBox_word.clear()
-        self.textBox_count.clear()
+        self.wordBox.clear()
+        self.countBox.clear()
         
 if __name__ == "__main__" : # 인터프리터에서 실행 할 경우
     app = QApplication(sys.argv) 
